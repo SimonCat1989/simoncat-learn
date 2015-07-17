484ceefc600565849c9cafe38b61cf291507974a
LUO Yihao <luo_y@worksap.co.jp>
2015-07-17 19:03:18 +0800

    [LUO Yihao] problem 1

---
+++ simoncat-learn-LeetCode/src/main/java/com/simoncat/learn/Q001_TwoSum/Solution.java
@@ -0,0 +1,8 @@
+package com.simoncat.learn.Q001_TwoSum;
+
+public class Solution {
+	
+	public int[] twoSum(int[] nums, int target) {
+		return null;
+	}
+}
---
+++ simoncat-learn-LeetCode/src/test/java/com/simoncat/learn/Q001_TwoSum/ResultTester.java
@@ -0,0 +1,11 @@
+package com.simoncat.learn.Q001_TwoSum;
+
+import org.junit.Test;
+
+public class ResultTester {
+
+	@Test
+	public void test() {
+		
+	}
+}
